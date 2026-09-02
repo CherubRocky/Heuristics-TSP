50cf6ebb65620c2fd0912c9c190838cdd5af42d2
Rocky <zar@ciencias.unam.mx>
2026-09-02 16:29:31 -0600

    feat: Implemented cost function in Instance

--- tsp_solver/instance.go
+++ tsp_solver/instance.go
@@ -12,7 +12,16 @@ type Instance struct {
 
 
 func (i *Instance) Cost(sol simulated_annealing.Solution) float64 {
-	return sol.Cost() // Implementation pending
+	var sum float64
+	tSol := sol.(*TravelSolution)
+	prev := 0
+	for index, id := range tSol.Permutation {
+		if index > 0 {
+			sum += i.Matrix.GetDistance(prev, id)
+		}
+		prev = id
+	}
+	return sum / i.Matrix.KEdgesSum
 }
 
 func (i *Instance) CostNeighbour(sol simulated_annealing.Solution, swap1 int, swap2 int) float64 {
