6901bf6fb74bea1da4a9d2a46800fdfafc72ee8d
Rocky <zar@ciencias.unam.mx>
2026-09-09 21:48:30 -0600

    feat: Implemented computeBatch and ThresholdAcceptance in simulated_annealing. Testing pending.

--- simulated_annealing/annealer.go
+++ simulated_annealing/annealer.go
@@ -1,8 +1,42 @@
 package simulated_annealing
 
-type annealer struct {
+import "math"
+
+type Annealer struct {
+	Epsilon float64
+	Temperature float64
+	ColdingFactor float64
+	BatchSize int
+	Best Solution
+}
+
+func (a *Annealer) computeBatch(s Solution) (float64, Solution) {
+	counter := 0
+	var sum float64 = 0.0
+	for counter < a.BatchSize {
+		neigh := s.GetNeighbour()
+		if neigh.Cost() <= s.Cost() + a.Temperature {
+			if neigh.Cost() < a.Best.Cost() {
+				a.Best = neigh
+			}
+			s = neigh
+			counter++
+			sum += s.Cost()
+		}
+	}
+	return sum / float64(a.BatchSize), s
 	
 }
 
-
-
+func (a *Annealer) ThresholdAcceptance(s Solution) Solution {
+	p := 0.0
+	for a.Temperature > a.Epsilon {
+		q := math.MaxFloat64
+		for p <= q {
+			q = p
+			p, s = a.computeBatch(s)
+		}
+		a.Temperature = a.ColdingFactor * a.Temperature
+	}
+	return a.Best
+}
