5efc81cd719ccc1f9594daba7eb5e6f2f7ca2a03
Rocky <zar@ciencias.unam.mx>
2026-09-11 08:38:19 -0600

    feat: Implemented neighbour method in instance

--- tsp_solver/instance.go
+++ tsp_solver/instance.go
@@ -1,5 +1,6 @@
 package tsp_solver
 import (
+	"slices"
 	"github.com/CherubRocky/Heuristics-TSP/simulated_annealing"
 	"github.com/CherubRocky/Heuristics-TSP/tsp"
 )
@@ -24,8 +25,71 @@ func (i *Instance) Cost(sol simulated_annealing.Solution) float64 {
 	return sum / i.Matrix.KEdgesSum// Modified this line
 }
 
-func (i *Instance) CostNeighbour(sol simulated_annealing.Solution, swap1 int, swap2 int) float64 {
-	return sol.Cost() // Implementation pending
+// Refactoring pending
+func (i *Instance) CostNeighbour(sol simulated_annealing.Solution, swap1 int, swap2 int) simulated_annealing.Solution {
+	tSol0 := sol.(*TravelSolution)
+	tSol := TravelSolution{slices.Clone(tSol0.Permutation), 0.0}
+	max, min := getMaxAndMin(swap1, swap2)
+	var maxEdges, minEdges, newEdges1, newEdges2 float64
+	if max == min + 1 {
+		if min > 0 {
+			minEdges = i.Matrix.GetDistance(tSol.Permutation[min], tSol.Permutation[min - 1])
+		}
+		if max < len(tSol.Permutation) - 1 {
+			maxEdges = i.Matrix.GetDistance(tSol.Permutation[max], tSol.Permutation[max + 1])
+		}
+		swap(max, min, tSol.Permutation)
+		if min > 0 {
+			newEdges1 = i.Matrix.GetDistance(tSol.Permutation[min], tSol.Permutation[min - 1])
+		}
+		if max < len(tSol.Permutation) - 1 {
+			newEdges2 = i.Matrix.GetDistance(tSol.Permutation[max], tSol.Permutation[max + 1])
+		}
+		
+	} else {
+		if max != len(tSol.Permutation) - 1 && min != 0 {
+			maxEdges = i.Matrix.GetDistance(tSol.Permutation[max], tSol.Permutation[max + 1]) + i.Matrix.GetDistance(tSol.Permutation[max], tSol.Permutation[max - 1])
+			minEdges = i.Matrix.GetDistance(tSol.Permutation[min], tSol.Permutation[min + 1]) + i.Matrix.GetDistance(tSol.Permutation[min], tSol.Permutation[min - 1])
+			swap(max, min, tSol.Permutation)
+			newEdges1 = i.Matrix.GetDistance(tSol.Permutation[max], tSol.Permutation[max + 1]) + i.Matrix.GetDistance(tSol.Permutation[max], tSol.Permutation[max - 1])
+			newEdges2 = i.Matrix.GetDistance(tSol.Permutation[min], tSol.Permutation[min + 1]) + i.Matrix.GetDistance(tSol.Permutation[min], tSol.Permutation[min - 1])
+		} else if max == len(tSol.Permutation) - 1 && min != 0 {
+			maxEdges = i.Matrix.GetDistance(tSol.Permutation[max], tSol.Permutation[max - 1])
+			minEdges = i.Matrix.GetDistance(tSol.Permutation[min], tSol.Permutation[min + 1]) + i.Matrix.GetDistance(tSol.Permutation[min], tSol.Permutation[min - 1])
+			swap(max, min, tSol.Permutation)
+			newEdges1 = i.Matrix.GetDistance(tSol.Permutation[max], tSol.Permutation[max - 1])
+			newEdges2 = i.Matrix.GetDistance(tSol.Permutation[min], tSol.Permutation[min + 1]) + i.Matrix.GetDistance(tSol.Permutation[min], tSol.Permutation[min - 1])
+		} else if max != len(tSol.Permutation) - 1 && min == 0 {
+			maxEdges = i.Matrix.GetDistance(tSol.Permutation[max], tSol.Permutation[max + 1]) + i.Matrix.GetDistance(tSol.Permutation[max], tSol.Permutation[max - 1])
+			minEdges = i.Matrix.GetDistance(tSol.Permutation[min], tSol.Permutation[min + 1])
+			swap(max, min, tSol.Permutation)
+			newEdges1 = i.Matrix.GetDistance(tSol.Permutation[max], tSol.Permutation[max + 1]) + i.Matrix.GetDistance(tSol.Permutation[max], tSol.Permutation[max - 1])
+			newEdges2 = i.Matrix.GetDistance(tSol.Permutation[min], tSol.Permutation[min + 1])
+		} else if max == len(tSol.Permutation) - 1 && min == 0 {
+			maxEdges = i.Matrix.GetDistance(tSol.Permutation[max], tSol.Permutation[max - 1])
+			minEdges = i.Matrix.GetDistance(tSol.Permutation[min], tSol.Permutation[min + 1])
+			swap(max, min, tSol.Permutation)
+			newEdges1 = i.Matrix.GetDistance(tSol.Permutation[max], tSol.Permutation[max - 1])
+			newEdges2 = i.Matrix.GetDistance(tSol.Permutation[min], tSol.Permutation[min + 1])
+		}
+	}
+	balance := (-(maxEdges + minEdges) + (newEdges1 + newEdges2)) / i.Normalizer
+	newCost := sol.Cost() + balance
+	tSol.SolutionCost = newCost
+	return &tSol
 }
 
 func (i *Instance) ComputeNormalizer() {}
+
+func getMaxAndMin(a int, b int) (int, int) {
+	if a > b {
+		return a, b
+	}
+	return b, a
+}
+
+func swap(a int, b int, sol []int) {
+	tmp := sol[a]
+	sol[a] = sol[b]
+	sol[b] = tmp
+}
