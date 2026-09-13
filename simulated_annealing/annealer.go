96083b3805c2ada02c85499d44af844259180a2b
Rocky <zar@ciencias.unam.mx>
2026-09-12 19:32:11 -0600

    feat: Implemented initial temperature function in annealer

--- simulated_annealing/annealer.go
+++ simulated_annealing/annealer.go
@@ -1,9 +1,13 @@
 package simulated_annealing
 
-//import "math"
+import (
+	"math"
+	"fmt"
+)
 
 type Annealer struct {
 	Epsilon float64
+	ProbEps float64
 	Temperature float64
 	ColdingFactor float64
 	BatchSize int
@@ -12,21 +16,25 @@ type Annealer struct {
 
 func (a *Annealer) computeBatch(s Solution) (float64, Solution) {
 	var sum float64 = 0.0
-	for step := 0; step < a.BatchSize; step++ {
+	counter := 0
+	for counter < a.BatchSize {
 		neigh := s.GetNeighbour()
 		if neigh.Cost() <= s.Cost() + a.Temperature {
 			if neigh.Cost() < a.Best.Cost() {
 				a.Best = neigh
 			}
+			fmt.Println("Accepted: ", neigh.Cost())
 			s = neigh
+			counter++
 			sum += s.Cost()
 		}
 	}
+	fmt.Println("Batch finished. Temperature: ", a.Temperature)
 	return sum / float64(a.BatchSize), s
 	
 }
 
-/*func (a *Annealer) ThresholdAcceptance(s Solution) Solution {
+func (a *Annealer) ThresholdAcceptance(s Solution) Solution {
 	p := 0.0
 	for a.Temperature > a.Epsilon {
 		q := math.MaxFloat64
@@ -37,8 +45,66 @@ func (a *Annealer) computeBatch(s Solution) (float64, Solution) {
 		a.Temperature = a.ColdingFactor * a.Temperature
 	}
 	return a.Best
-        }*/
-func (a *Annealer) ThresholdAcceptance(s Solution) Solution {
+}
+
+func (a *Annealer) InitialTemp(s Solution, p float64) {
+	var t1, t2 float64
+	prob := a.AcceptedPercent(s, a.Temperature)
+	if math.Abs(p - prob) <= a.ProbEps {
+		return
+	}
+	if prob < p {
+		for prob < p {
+			a.Temperature *= 2
+			prob = a.AcceptedPercent(s, a.Temperature)
+		}
+		t1 = a.Temperature / 2
+		t2 = a.Temperature
+	} else {
+		for prob > p {
+			a.Temperature = a.Temperature / 2
+			prob = a.AcceptedPercent(s, a.Temperature)
+		}
+		t1 = a.Temperature
+		t2 = a.Temperature * 2
+	}
+	a.Temperature = a.BinarySearch(s, t1, t2, p)
+}
+
+func (a *Annealer) AcceptedPercent(s Solution, t float64) float64 {
+	counter := 0
+	n := a.BatchSize
+	for i := 0; i < n; i++ {
+		neigh := s.GetNeighbour()
+		if neigh.Cost() <= s.Cost() + t {
+			if neigh.Cost() < a.Best.Cost() {
+				a.Best = neigh
+			}
+			counter++
+			s = neigh
+		}
+	}
+	return float64(counter) / float64(n)
+}
+
+func (a *Annealer) BinarySearch(s Solution, t1 float64, t2 float64, p float64) float64 {
+	meanT := (t1 + t2) / 2
+	if t2 - t1 < a.ProbEps {
+		return meanT
+	}
+	prob := a.AcceptedPercent(s, meanT)
+	if math.Abs(p - prob) < a.ProbEps {
+		return meanT
+	}
+	if prob > p {
+		return a.BinarySearch(s, t1, meanT, p)
+	} else {
+		return a.BinarySearch(s, meanT, t2, p)
+	}
+	
+}
+
+/*func (a *Annealer) ThresholdAcceptance(s Solution) Solution {
 	if a.Best == nil {
 		a.Best = s
 	}
@@ -48,4 +114,4 @@ func (a *Annealer) ThresholdAcceptance(s Solution) Solution {
 		a.Temperature = a.ColdingFactor * a.Temperature
 	}
 	return a.Best
-}
+        }*/
--- tsp_solver/solver.go
+++ tsp_solver/solver.go
@@ -9,11 +9,13 @@ func (i *Instance) Solve(ids []int) simulated_annealing.Solution {
 	initSolCost := i.Cost(&initialSol)
 	initialSol.SolutionCost = initSolCost
 	ann := simulated_annealing.Annealer{
-		Epsilon: 0.00001,
-		Temperature: 0.5,
-		ColdingFactor: 0.9,
+		Epsilon: 0.00005,
+		ProbEps: 0.05,
+		Temperature: 8.0,
+		ColdingFactor: 0.9999,
 		BatchSize: 2000,
 		Best: &initialSol,
 	}
+	ann.InitialTemp(&initialSol, 0.87)
 	return ann.ThresholdAcceptance(&initialSol)
 }
